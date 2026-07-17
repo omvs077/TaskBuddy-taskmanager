c4150c11b11a6b4a8bb7764de5463065b59f87a9
omvs077 <omvs077@gmail.com>
2026-07-17 16:33:30 +0530

    [Milestone 2.1] Dear ImGui + DirectX 11 basic UI rendering

---
+++ src/AppWindow.cpp
@@ -0,0 +1,124 @@
+﻿#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#include <d3d11.h>
+#include <windows.h>
+#include <tchar.h>
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND, UINT, WPARAM, LPARAM);
+
+static ID3D11Device* g_pd3dDevice = nullptr;
+static ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
+static IDXGISwapChain* g_pSwapChain = nullptr;
+static ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
+
+static bool CreateDeviceD3D(HWND hWnd) {
+    DXGI_SWAP_CHAIN_DESC sd{};
+    sd.BufferCount = 2;
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    UINT createDeviceFlags = 0;
+    D3D_FEATURE_LEVEL featureLevel;
+    const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0 };
+    HRESULT res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags,
+        featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
+    if (res != S_OK) return false;
+
+    ID3D11Texture2D* pBackBuffer;
+    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+    pBackBuffer->Release();
+    return true;
+}
+
+static void CleanupDeviceD3D() {
+    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = nullptr; }
+    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = nullptr; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
+}
+
+extern LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam)) return true;
+    switch (msg) {
+    case WM_SIZE:
+        if (g_pd3dDevice != nullptr && wParam != SIZE_MINIMIZED) {
+            if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+            ID3D11Texture2D* pBackBuffer;
+            g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+            g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+            pBackBuffer->Release();
+        }
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int RunAppWindow() {
+    WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr),
+        nullptr, nullptr, nullptr, nullptr, L"TaskBuddyWndClass", nullptr };
+    RegisterClassExW(&wc);
+    HWND hwnd = CreateWindowW(wc.lpszClassName, L"TaskBuddy", WS_OVERLAPPEDWINDOW,
+        100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
+
+    if (!CreateDeviceD3D(hwnd)) { CleanupDeviceD3D(); UnregisterClassW(wc.lpszClassName, wc.hInstance); return 1; }
+
+    ShowWindow(hwnd, SW_SHOWDEFAULT);
+    UpdateWindow(hwnd);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+    bool done = false;
+    while (!done) {
+        MSG msg;
+        while (PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+            if (msg.message == WM_QUIT) done = true;
+        }
+        if (done) break;
+
+        ImGui_ImplDX11_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("TaskBuddy");
+        ImGui::Text("Phase 2.1: UI shell online.");
+        ImGui::End();
+
+        ImGui::Render();
+        const float clearColor[4] = { 0.08f, 0.08f, 0.10f, 1.0f };
+        g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, nullptr);
+        g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clearColor);
+        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+        g_pSwapChain->Present(1, 0);
+    }
+
+    ImGui_ImplDX11_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+    CleanupDeviceD3D();
+    DestroyWindow(hwnd);
+    UnregisterClassW(wc.lpszClassName, wc.hInstance);
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,7 @@
 ﻿#include <windows.h>
-#include <cstdio>
-#include <vector>
-#include "ProcessInfo.h"
 
-typedef NTSTATUS(WINAPI* NtQuerySystemInformation_t)(ULONG, PVOID, ULONG, PULONG);
+int RunAppWindow();
 
-std::vector<ProcessInfo> SnapshotToProcessInfo(BYTE* buffer) {
-    std::vector<ProcessInfo> result;
-    auto* entry = reinterpret_cast<TB_SYSTEM_PROCESS_INFORMATION*>(buffer);
-    while (true) {
-        ProcessInfo info{};
-        info.Pid = (uint32_t)(uintptr_t)entry->UniqueProcessId;
-        info.ParentPid = (uint32_t)(uintptr_t)entry->InheritedFromUniqueProcessId;
-        info.WorkingSetBytes = entry->WorkingSetSize;
-        info.CpuTime100ns = (uint64_t)entry->KernelTime.QuadPart + (uint64_t)entry->UserTime.QuadPart;
-        info.ImageName = entry->ImageName.Buffer ? entry->ImageName.Buffer : L"[System Idle/Unknown]";
-        result.push_back(std::move(info));
-
-        if (entry->NextEntryOffset == 0) break;
-        entry = reinterpret_cast<TB_SYSTEM_PROCESS_INFORMATION*>(
-            reinterpret_cast<BYTE*>(entry) + entry->NextEntryOffset);
-    }
-    return result;
-}
-
-int main() {
-    auto NtQuerySystemInformation = (NtQuerySystemInformation_t)GetProcAddress(
-        GetModuleHandleW(L"ntdll.dll"), "NtQuerySystemInformation");
-    if (!NtQuerySystemInformation) { fprintf(stderr, "Failed to resolve NtQuerySystemInformation\n"); return 1; }
-
-    ULONG bufSize = 1 << 16;
-    std::vector<BYTE> buffer;
-    NTSTATUS status;
-    do {
-        buffer.resize(bufSize);
-        status = NtQuerySystemInformation(5, buffer.data(), bufSize, &bufSize);
-        if (status == 0xC0000004) bufSize *= 2;
-    } while (status == 0xC0000004);
-
-    if (status < 0) { fprintf(stderr, "NtQuerySystemInformation failed: 0x%08X\n", status); return 1; }
-
-    auto processes = SnapshotToProcessInfo(buffer.data());
-    for (const auto& p : processes)
-        wprintf(L"PID: %-6u  PPID: %-6u  WS: %8llu KB  %s\n",
-                p.Pid, p.ParentPid, p.WorkingSetBytes / 1024, p.ImageName.c_str());
-
-    wprintf(L"\nTotal processes: %zu\n", processes.size());
-    return 0;
+int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    return RunAppWindow();
 }
