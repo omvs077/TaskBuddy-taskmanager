8e5a05d4077c9b8786e4430ad73366d823fce4c2
omvs077 <omvs077@gmail.com>
2026-08-26 16:04:36 +0530

    [v2 Milestone 13.0] Startup Impact color grading: High/Medium/Low get red/amber/green cell backgrounds via new StartupImpactBrushConverter, matching the CPU/Memory/Disk heat-map pattern already used on Processes tab.

---
+++ TaskBuddyWPF/TaskBuddyWPF/Converters/StartupImpactBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using TaskBuddyWPF.Models;
+
+namespace TaskBuddyWPF.Converters
+{
+    public class StartupImpactBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var impact = value is StartupImpact i ? i : StartupImpact.NotMeasured;
+            return impact switch
+            {
+                StartupImpact.High => new SolidColorBrush(Color.FromArgb(60, 232, 65, 65)),
+                StartupImpact.Medium => new SolidColorBrush(Color.FromArgb(60, 232, 160, 30)),
+                StartupImpact.Low => new SolidColorBrush(Color.FromArgb(45, 90, 200, 90)),
+                _ => Brushes.Transparent // NotMeasured, None
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
