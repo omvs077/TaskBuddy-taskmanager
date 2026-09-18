caffafcbb6869bb520432dd6495a8d73b749e54a
omvs077 <omvs077@gmail.com>
2026-09-18 10:37:52 +0530

    [Polish 4.1d] Add collapsible group headers (Apps/Background/Windows processes) and child-cluster indentation on Processes tab

---
+++ TaskBuddyWPF/TaskBuddyWPF/Converters/IndentLevelToMarginConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TaskBuddyWPF.Converters
+{
+    public class IndentLevelToMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int level = value is int i ? i : 0;
+            return new Thickness(level * 20, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
